213f98c6bb224e0bc00c709b27988da8981c70f2
AnuragVish1 <anuragvishwakarmaalt@gmail.com>
2025-05-11 00:39:52 +0530

    import fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
