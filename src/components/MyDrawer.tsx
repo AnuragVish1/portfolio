b4bdf8948fc9c1e7fa9221cbe0ffac0fcf62c539
AnuragVish1 <anuragvishwakarmaalt@gmail.com>
2025-11-02 01:41:15 +0530

    added flutter as skill and new project

--- src/components/MyDrawer.tsx
+++ src/components/MyDrawer.tsx
@@ -9,34 +9,104 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
 
-export const MyDrawer = ({ children, image, description, title }) => {
+
+
+export const MyDrawer = ({ children, image, description, title, images }) => {
+
+  const [isOverlyClick, setIsOverlyClick] = useState(false);
+
+  useEffect(() => {
+    console.log(isOverlyClick);
+  }, [isOverlyClick]);
+
+  useEffect(() => {
+    setIsOverlyClick(false);
+
+    return () => {
+      setIsOverlyClick(false);
+    }
+  }, []);
+
   return (
-    <Drawer>
-      <DrawerTrigger
-        className="text-left w-full
-        "
-      >
-        {children}
-      </DrawerTrigger>
-      <DrawerContent className="bg-[#f9f3e9]">
-        <div className="p-10 font-inter overflow-scroll md:overflow-hidden">
-          <div className=" flex gap-6 md:flex-row flex-col ">
-            <img
-              src={image}
-              alt=""
-              className=" rounded-xl h-[15rem]  mt-5 object-cover object-center"
-            />
-            <div className="flex flex-col justify-between">
-              <h1 className="md:text-[4rem] text-[2.5rem] font-bold">{title}</h1>
+    <>
+      <Drawer>
 
-              <p className=" pt-5 text-2xl w-full text-justify overflow-scroll md:overflow-hidden">
-                {description}
-              </p>
+        <DrawerTrigger
+          className="text-left w-full
+        "
+        >
+          {children}
+        </DrawerTrigger>
+        <DrawerContent className="bg-[#f9f3e9]" onInteractOutside={(e) => {
+          if (isOverlyClick) {
+            e.preventDefault();
+          }
+        }}
+        >
+          {isOverlyClick && images.length != 0 &&  (
+            <div className=" bg-[#0000004b] fixed h-full w-full inset-0 z-[100] p-10 backdrop-blur-sm rounded-lg" onClick={(e) => {
+              e.stopPropagation()
+            }}>
+              {/*navbar*/}
+              <div className="flex justify-between flex-row w-full">
+                <h1 className=" text-4xl text-white font-bold">{title}</h1>
+                <button className="bg-[#f6f6f6e1] size-auto rounded-sm p-2 active:scale-90" onClick={(e) => {
+                  e.stopPropagation();
+                  setIsOverlyClick(false)
+                  console.log(isOverlyClick)
+                }}>
+                  <X />
+                </button>
+              </div>
+              {/*carousel*/}
+              <div className="w-full h-full flex justify-center items-center">
+                <Carousel className="md:w-[50%] w-[95%]">
+                  <CarouselContent>
+                    {images.map((image, _) => (
+                      <CarouselItem key={_}>
+                        <img src={image} alt="" draggable="false" className="rounded-lg z-100 h-[22rem] object-contain" />
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
+              </div>
             </div>
+          )}
+
+          <div className="p-10 font-inter ">
+
+            <div className=" flex gap-6 md:flex-row flex-col ">
+
+
+              <img
+                src={image}
+                alt=""
+                className=" rounded-xl h-[19rem]  mt-5 object-contain"
+                onClick={(e) => { if (images.length != 0) { setIsOverlyClick(true) }}}
+              />
+
+
+
+              <div className="flex flex-col justify-between">
+                <h1 className="md:text-[4rem] text-[2.5rem] font-bold">{title}</h1>
+
+                <p className=" pt-5 text-2xl w-full text-justify overflow-scroll md:overflow-hidden">
+                  {description}
+                </p>
+              </div>
+            </div>
+          
           </div>
-        </div>
-      </DrawerContent>
-    </Drawer>
+        </DrawerContent>
+      </Drawer>
+
+    </>
   );
 };
